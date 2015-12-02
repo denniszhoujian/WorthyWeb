3776c81e422932759a6b5302519fdebc85cece80
dennis <271393@qq.com>
2015-12-03 02:45:23 +0800

    improve

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ $URL_HOST = "http://192.168.31.106";
 $URL_JSONP = "http://192.168.31.106:8080";
 
 /** PROD ENV */
-//$URL_HOST = "http://www.moshutao.com";
+//$URL_HOST = "http://www.moshutao.com:9090";
 //$URL_JSONP = "http://www.moshutao.com:8090";
 
 
